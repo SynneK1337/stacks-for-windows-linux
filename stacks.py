0e1af8627b7129927a50ed72a7ce8bcd264a0809
synnek1337 <synnek1337@protonmail.com>
2019-03-24 01:29:57 +0100

    Add support of video files, script now removes empty folders

--- stacks.py
+++ stacks.py
@@ -18,29 +18,42 @@ file_type_by_extension = {'images':
                            '.fff', '.mef', '.mos', '.mrw', '.nef', '.nrw',        # RAW
                            '.orf', '.ptx', '.pef', '.pxn', '.r3d', '.raf',        # RAW
                            '.raw', '.rw2', '.rw1', '.rwz', '.x3f'                 # RAW
-                           ]
+                           ],
+                          'videos':
+                          ['.webm', '.mkv', '.flv', '.vob', '.ogv' '.ogg',
+                           '.drc', '.gifv', '.mng', '.avi', '.mts', '.m2ts',
+                           '.mov', '.qt', '.wmv', '.yuv', '.rm', '.rmvb',
+                           '.asf', '.amv', '.mp4', '.m4p', '.m4v', '.mpg',
+                           '.mp2', '.mpeg', '.mpe', '.mpv', '.m2v', '.m4v',
+                           '.svi', '.3gp', '.3g2', '.mxf', '.roq', '.nsv',
+                           '.fl4', '.f4p', '.f4v', '.f4a', '.f4b'
+                           ],
                           }
 
 
 def get_desktop_path():
-  return os.path.expanduser("~/Desktop")
+    return os.path.expanduser("~/Desktop")
 
 
 def get_file_type(file, types):  # types = file_type_by_extension
-  for type, extension in types.items():
-    if pathlib.Path(file).suffix.lower() in extension:
-      return type
+    for type, extension in types.items():
+        if pathlib.Path(file).suffix.lower() in extension:
+            return type
 
 
 def create_folders(types):     # types = file_type_by_extension
-  for type_ in types:
-    os.makedirs(type_, exist_ok=True)
+    for type_ in types:
+        os.makedirs(type_, exist_ok=True)
 
 
 if __name__ == "__main__":
-  os.chdir(get_desktop_path())  # cd ~/Desktop
-  create_folders(file_type_by_extension)
-  files = os.listdir()   # ls
-  for file in files:
-    if get_file_type(file, file_type_by_extension):
-      os.rename(file, os.path.join(get_file_type(file, file_type_by_extension), file))
+    os.chdir(get_desktop_path())  # cd ~/Desktop
+    create_folders(file_type_by_extension)
+    files = os.listdir()   # ls
+    for file in files:
+        if get_file_type(file, file_type_by_extension):
+            os.rename(file, os.path.join(get_file_type(file,
+                                                       file_type_by_extension), file))
+    for folder in file_type_by_extension:
+        if not os.listdir(folder):
+            os.removedirs(folder)
